aa99c3ac39ce488ea86c250f8e52029bb35b32d5
Nicolás Narria <nnarria@gmail.com>
2016-05-02 23:05:38 -0300

    Tarea Paseando en bicicleta
    App que registra un punto usando anotaciones cada 50 mts pudiendo
    cambiar la vista del tipo de mapa, registrando la distancia acumulada,
    pudiendo reiniciar la ruta.

--- TareaPaseandoEnBicicleta/ViewController.swift
+++ TareaPaseandoEnBicicleta/ViewController.swift
@@ -7,19 +7,149 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapa: MKMapView!
+    @IBOutlet weak var distanciaText: UITextField!
+    
+    private let manejador = CLLocationManager()
+    private var distanciaAcumulada = 0.0
+    private var posicionRefencia = CLLocation()
+    
+    private var lecturaIniciada = false
+    private var primerAlfiler = false
+    private let RANGO_METRO = 50.0
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        manejador.delegate = self
+        manejador.desiredAccuracy = kCLLocationAccuracyBest
+        manejador.requestWhenInUseAuthorization()
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        
+        if status == .AuthorizedWhenInUse {
+            manejador.startUpdatingLocation()
+            mapa.showsUserLocation = true
+            
+            mapa.mapType = MKMapType.Standard
+        }
+        else {
+            manejador.stopUpdatingLocation()
+            mapa.showsUserLocation = false
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var distanciaEnMetrosRef = 0.0
+        
+        if (lecturaIniciada == false) {
+            var region = MKCoordinateRegion()
+            var location = CLLocationCoordinate2D()
+            var span = MKCoordinateSpan()
+            span.latitudeDelta = 0.08
+            span.longitudeDelta = 0.08
+
+            
+            location.latitude = manejador.location!.coordinate.latitude;
+            location.longitude = manejador.location!.coordinate.longitude;
+            region.span = span;
+            region.center = location;
+
+            //inicio punto referencia
+            posicionRefencia = manager.location!
+            mapa.setRegion(region, animated: true)
+            
+            //para que te siga
+            mapa.setUserTrackingMode(MKUserTrackingMode.Follow, animated: true)
+            lecturaIniciada = true
+        }
+        else {
+        
+            let tmp_loc = manager.location
+            distanciaEnMetrosRef = posicionRefencia.distanceFromLocation(tmp_loc!)
+            
+            //una vez
+            if (distanciaEnMetrosRef >= RANGO_METRO && primerAlfiler == false) {
+                distanciaAcumulada = RANGO_METRO
+                primerAlfiler = true
+                agregarAlfiler(tmp_loc!)
+            }
+            else if (distanciaEnMetrosRef >= RANGO_METRO){
+                distanciaAcumulada += distanciaEnMetrosRef
+                agregarAlfiler(tmp_loc!)
+            }
+            else {
+                print("continuo calculando: \(distanciaEnMetrosRef)")
+            }
+            
+            distanciaText.text = "\(round(distanciaAcumulada))" + " Mts"
+        }
+    }
+    
+    func agregarAlfiler (loc: CLLocation) {
+        print("Debo agregar alfiler")
+        var punto = CLLocationCoordinate2D()
+        punto.latitude = (loc.coordinate.latitude)
+        punto.longitude = (loc.coordinate.longitude)
+        
+        let pin = MKPointAnnotation()
+        pin.title = "\(punto.latitude), \(punto.longitude)"
+        pin.subtitle = "\(distanciaAcumulada)"
+        pin.coordinate = punto
+        
+        mapa.addAnnotation(pin)
+        posicionRefencia = loc
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    
+    @IBAction func cambiarTipoMapa(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex
+        {
+        case 0:
+            print ("Map Standard selected")
+            mapa.mapType = MKMapType.Standard
+            break
+        case 1:
+            print ("Map Hybrid selected")
+            mapa.mapType = MKMapType.Hybrid
+            break
+        case 2:
+            print ("Map Satellite selected")
+            mapa.mapType = MKMapType.Satellite
+            break
+        default:
+            break; 
+        }
     }
-
-
+    
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        let alerta = UIAlertController (title: "ERROR", message: "error \(error.code)", preferredStyle:.Alert)
+        let accionOk = UIAlertAction (title: "OK", style: .Default, handler:
+            {accion in
+                //...
+        })
+        
+        alerta.addAction(accionOk)
+        self.presentViewController(alerta, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func resetRutaActual() {
+        print ("se limpia el mapa2")
+        distanciaAcumulada = 0.0
+        mapa.removeAnnotations(mapa.annotations)
+        primerAlfiler = false
+        distanciaText.text = "0.0 Mts"
+    }
+    
 }
 
